40dbd4730e2cd928dafe68234e68146b4b31bfa6
Maksim Gladki <maks.falonse@gmail.com>
2026-02-26 14:59:23 +0300

    feat: add custom hook for saving data in localStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+    const [value, setValue] = useState<T>(() => {
+        try {
+            const item = localStorage.getItem(key);
+            return item ? JSON.parse(item) as T : initialValue;
+        } catch (err: unknown) {
+            console.warn(`Error loading state from localStorage for key ${key}:`, err);
+            return initialValue;
+        }
+    });
+
+    useEffect(() => {
+        try {
+            localStorage.setItem(key, JSON.stringify(value));
+        } catch (err: unknown) {
+            console.warn(`Error saving state to localStorage for key ${key}:`, err);
+        }
+    }, [value]);
+
+    return [value, setValue] as const;
+};
+
+export default useLocalStorage;
