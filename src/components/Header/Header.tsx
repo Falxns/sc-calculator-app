a22445084952f76fe5d4050a7bb7a131b344794f
Maksim Gladki <maks.falonse@gmail.com>
2026-02-26 15:37:49 +0300

    feat: add basic Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-gray-700 text-gray-200 p-2 flex justify-center items-center gap-2">
+            <h1 className="text-4xl font-bold">Stalcraft:X Materials Calculator</h1>
+        </header>
+    );
+};
+
+export default Header;
