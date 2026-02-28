4f0df2bcd8b0ce4b9721fb2c30308872cba46519
Maksim Gladki <maks.falonse@gmail.com>
2026-02-28 23:39:53 +0300

    feat: add validation and clear button

--- src/components/PriceCalculator/PriceCalculator.tsx
+++ src/components/PriceCalculator/PriceCalculator.tsx
@@ -16,7 +16,7 @@ const PriceCalculator = () => {
     <div className="flex justify-center items-center gap-2 bg-gray-700 text-gray-200 p-2 rounded-2xl">
       <img src={slastena} alt="Slastena" className="w-16 h-16" />
       <input
-        className="text-base text-black text-center p-2 rounded-md w-24 border-2 border-gray-500 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="text-base text-black text-center p-2 rounded-md w-24 border-2 border-gray-500 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-500"
         type="number"
         value={calculatorState.price === 0 ? '' : calculatorState.price}
         placeholder="Price"
@@ -28,9 +28,14 @@ const PriceCalculator = () => {
             total: nextPrice * calculatorState.quantity,
           });
         }}
+        onKeyDown={(e) => {
+          if (['-', 'e', 'E', '+'].includes(e.key)) {
+            e.preventDefault();
+          }
+        }}
       />
       <input
-        className="text-base text-black text-center p-2 rounded-md w-24 border-2 border-gray-500 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="text-base text-black text-center p-2 rounded-md w-24 border-2 border-gray-500 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-500"
         type="number"
         value={calculatorState.quantity === 0 ? '' : calculatorState.quantity}
         placeholder="Quantity"
@@ -42,8 +47,25 @@ const PriceCalculator = () => {
             total: calculatorState.price * nextQuantity,
           });
         }}
+        onKeyDown={(e) => {
+          if (['-', 'e', 'E', '+'].includes(e.key)) {
+            e.preventDefault();
+          }
+        }}
       />
       <p className="text-base font-bold">Total: {calculatorState.total}</p>
+      <button
+        className="text-base text-black text-center p-2 rounded-md w-20 border-2 border-gray-500 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => {
+          setCalculatorState({
+            price: 0,
+            quantity: 0,
+            total: 0,
+          });
+        }}
+      >
+        Clear
+      </button>
     </div>
   );
 };
